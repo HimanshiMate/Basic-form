75bf791ce54c315431b7447f1d8c3ba06f6b2839
Himanshi Mate <matemanu725@gmail.com>
2024-08-22 22:16:11 +0530

    remark

---
+++ project/app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import stu_Ragistration
+
+class Ragistration(forms.ModelForm):
+    # fname=forms.CharField(max_length=50,label="first name")
+    # lname=forms.CharField(max_length=20,label="last name")
+    # email=forms.EmailField(label="email")
+    # contact=forms.IntegerField(label="contact")
+    class Meta:
+        model=stu_Ragistration
+        fields='__all__'
---
+++ project/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .forms import *
+
+# Create your views here.
+
+
+def home(request):
+    form=Ragistration()
+    # return render(request,"home.html",{"form":form})
+    if request.method=="POST":
+        form=Ragistration(request.POST)
+        
+        if form.is_valid():
+            fname=form.cleaned_data["fname"]  #in square bracket form variable is written there
+            lname=form.cleaned_data["lname"]
+            email=form.cleaned_data["email"]
+            contact=form.cleaned_data["contact"]
+            print(fname,lname,email,contact)
+            form.save()
+            # data={"fname":fname,"lname":lname,"email":email,"contact":contact}
+    return render(request,"home.html",{"form":form})
